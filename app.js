b049ffcc9347bff894938c9fc37b393a8a0e862f
Josue Efrain Avelar Dominguez <joshy_je@hotmail.com>
2022-04-21 22:33:35 -0500

    Agregando una nueva ruta

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/',(req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/launchx',(req,res) => {
+    res.send('Bienvenidos a Launch X')
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
     console.log('Example app listening on port ${port}')
